051a1528c6a040eff16c1d50cc4ee8a961af95f3
Sampo Toiva <sampo.toiva@gmail.com>
2012-12-11 19:02:33 +0200

    Fixed tab indenting.

--- public/scripts/app/models/exercise.js
+++ public/scripts/app/models/exercise.js
@@ -1,67 +1,67 @@
 define([ 'spine/spine'
-	   , 'spine/local'
-	   , 'app/models/exercise_types']
-	   , function(Spine, Local, ExerciseTypes) {
+       , 'spine/local'
+       , 'app/models/exercise_types']
+       , function(Spine, Local, ExerciseTypes) {
 
-	var Exercise = Spine.Model.sub();
-	Exercise.configure("Exercise", "sets", "reps", "weight", "date", "type");
-	Exercise.extend(Spine.Model.Local);
+    var Exercise = Spine.Model.sub();
+    Exercise.configure("Exercise", "sets", "reps", "weight", "date", "type");
+    Exercise.extend(Spine.Model.Local);
 
-	Exercise.include({
-		validate: function(){
-			if( !this.sets ){
-				return "Sets are required";
-			}else if( !this.reps ){
-				return "Reps are required";
-			}else if( !this.weight ){
-				return "Weight is required";
-			}else if( !this.type ){
-				return "Type is required";
-			}else if( ExerciseTypes.types.indexOf(this.type) == -1 ){
-				return "Invalid type";
-			}else if( !this.date ){
-				return "Date is required"
-			}
-			//should check if date is valid
-		},
-
-		getMonth: function(){
-			return Exercise.parseMonth(this.date);
-		}
-	});
-
-	Exercise.extend({
-		//Calculates 1RM (in kg) according to the formula by Epley: http://athletics.wikia.com/wiki/Epley_Formula
-		//Rounds to closest multiple of 1.25 kg 
-		calculateOneRm: function(exercise){
-			return 1.25*(Math.round(((exercise.reps * exercise.weight / 30) + exercise.weight)/1.25));
-		}
-
-		//parses month from date and returns it as int
-		, parseMonth: function(date){
-			var dateArr = date.split('.')
-			return parseInt(dateArr[1]);
-		}
-
-		, fetchAllByMonth: function(month){
-			return Exercise.select(function(exercise){
-				return month === exercise.getMonth(); 
-			});
-		}
-
-		, compare: function(exA, exB){
-			if(a.date > b.date){
-            	return -1;
-            }else if(a.date < b.date){
-            	return 1
-            }else{
-            	return 0;
+    Exercise.include({
+        validate: function(){
+            if( !this.sets ){
+                return "Sets are required";
+            }else if( !this.reps ){
+                return "Reps are required";
+            }else if( !this.weight ){
+                return "Weight is required";
+            }else if( !this.type ){
+                return "Type is required";
+            }else if( ExerciseTypes.types.indexOf(this.type) == -1 ){
+                return "Invalid type";
+            }else if( !this.date ){
+                return "Date is required"
             }
-		}
+            //should check if date is valid
+        },
 
-		//Combines exercises by date in an exercise array
-		//Data format: [{date: '26.12.2012', exercises: [{type: "squat", reps: 5, sets: 3, weight: 150}]}]
-		, combineExercisesByDate: function(exercises){ 
+        getMonth: function(){
+            return Exercise.parseMonth(this.date);
+        }
+    });
+
+    Exercise.extend({
+        //Calculates 1RM (in kg) according to the formula by Epley: http://athletics.wikia.com/wiki/Epley_Formula
+        //Rounds to closest multiple of 1.25 kg 
+        calculateOneRm: function(exercise){
+            return 1.25*(Math.round(((exercise.reps * exercise.weight / 30) + exercise.weight)/1.25));
+        }
+
+        //parses month from date and returns it as int
+        , parseMonth: function(date){
+            var dateArr = date.split('.')
+            return parseInt(dateArr[1]);
+        }
+
+        , fetchAllByMonth: function(month){
+            return Exercise.select(function(exercise){
+                return month === exercise.getMonth(); 
+            });
+        }
+
+        , compare: function(exA, exB){
+            if(a.date > b.date){
+                return -1;
+            }else if(a.date < b.date){
+                return 1
+            }else{
+                return 0;
+            }
+        }
+
+        //Combines exercises by date in an exercise array
+        //Data format: [{date: '26.12.2012', exercises: [{type: "squat", reps: 5, sets: 3, weight: 150}]}]
+        , combineExercisesByDate: function(exercises){ 
       
             if(!exercises || exercises.length === 0){
                 return []; //return an empty array if called with undefined or empty array
@@ -76,8 +76,8 @@ define([ 'spine/spine'
             var entriesIdx = 0;
             for(var i = 0; i < exercises.length; i++){
 
-            	//take the first logEntry
-            	var logEntry = {  type: exercises[i].type
+                //take the first logEntry
+                var logEntry = {  type: exercises[i].type
                                 , reps: exercises[i].reps
                                 , sets: exercises[i].sets
                                 , weight: exercises[i].weight };
@@ -95,10 +95,10 @@ define([ 'spine/spine'
 
             return entries;
         }
-	
-	})
+    
+    });
 
 
-	return Exercise;
+    return Exercise;
 
 });
